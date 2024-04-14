dfac04c91a0feb93b8570e0ea6b4c0fe93f012fa
Your Name <you@example.com>
2024-04-14 15:23:22 +0200

    fix: Maven url

--- build.gradle.kts
+++ build.gradle.kts
@@ -83,7 +83,7 @@ publishing {
     repositories {
         maven {
             name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/revanced/revanced-patches-template")
+            url = uri("https://maven.pkg.github.com/1fexd/revanced-patches")
             credentials {
                 username = System.getenv("GITHUB_ACTOR")
                 password = System.getenv("GITHUB_TOKEN")
